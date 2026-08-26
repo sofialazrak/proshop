f93fe783bf8e72dd93cc83878e74804882296106
Sofia Lazrak <sofia.lazrak@gmail.com>
2026-08-26 18:46:35 +0100

    Fourth  commit correction 2

--- auth.config.ts
+++ auth.config.ts
@@ -1,8 +1,6 @@
 // auth.config.ts
 
 import type { NextAuthConfig } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import { NextResponse } from "next/server";
 
 export default {
   pages: {
@@ -14,29 +12,7 @@ export default {
     strategy: "jwt",
     maxAge: 30 * 24 * 60 * 60,
   },
-
-  providers: [
-    CredentialsProvider({
-      credentials: {
-        email: { type: "email" },
-        password: { type: "password" },
-      },
-    }),
-  ],
-
-  callbacks: {
-    authorized({ request, auth }) {
-      if (!request.cookies.get("sessionCartId")) {
-        const sessionCartId = crypto.randomUUID();
-
-        const response = NextResponse.next();
-
-        response.cookies.set("sessionCartId", sessionCartId);
-
-        return response;
-      }
-
-      return true;
-    },
-  },
+  trustHost: true,
+  providers: [],
+  callbacks: {},
 } satisfies NextAuthConfig;
--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,15 @@
-// import { auth } from "@/auth";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-// export default auth;
+export default function middleware(request: NextRequest) {
+  const response = NextResponse.next();
 
-// middleware.ts
+  if (!request.cookies.get("sessionCartId")) {
+    response.cookies.set("sessionCartId", crypto.randomUUID());
+  }
 
-import NextAuth from "next-auth";
-import authConfig from "./auth.config";
-
-export const { auth } = NextAuth(authConfig);
-
-export default auth;
+  return response;
+}
 
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
